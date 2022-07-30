c6796782a8976a73abca0245edfbdca48a7cd2a9
Jasonparra21 <jeissonparra1212@gmail.com>
2022-07-30 15:14:41 -0500

    Se agrega .gitignore y los arcivos de lo trabajado

---
+++ indexnew.js
@@ -0,0 +1,78 @@
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btncalcular');
+const presult = document.querySelector('#result');
+
+
+form.addEventListener('submit', sumarvalores); 
+
+function sumarvalores(event) {
+    event.preventDefault();
+    console.log('Escuchando');
+    const sumA = input1.value + input2.value;
+    presult.innerText = "Resultado " + sumA;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////// CLASE ESCUCHAR ////////////////////////////////////777
+
+// const h1 = document.querySelector('h1');
+// const input1 = document.querySelector('#calculo1');
+// const input2 = document.querySelector('#calculo2');
+// const btn = document.querySelector('#btncalcular');
+// const presult = document.querySelector('#result');
+
+// function btnonclick() {
+//     console.log('Escuchando');
+//     const sumA = input1.value + input2.value;
+//     presult.innerText = "Resultado " + sumA;
+// }
+
+
+/////////////////// otra clase //////////////////////////////////////77
+
+// const p = document.querySelector('p');
+// const parrafito = document.getElementsByClassName('parrafito');
+// const pid = document.getElementById('pid');
+
+
+
+// console.log(input.value);
+
+// console.log({
+//     h1,
+//     p,
+//     parrafito,
+//     pid,
+//     input,
+// });
+
+// h1.innerHTML='Puta <br> Feo';
+// h1.innerText='Si aprendi';
+// //console.log(h1.getAttribute('pantalla'));
+// //h1.setAttribute('pantalla','ROJO');
+// h1.classList.add('AZUL');
+// h1.classList.remove('AZUL');
+
+// input.value=456
+ 
+// const img =document.createElement('img');
+// img.setAttribute('src','https://static.wixstatic.com/media/90b5fe_c5aad61b20ce472ea7cc77350f978e5a~mv2_d_2651_1818_s_2.jpg/v1/fill/w_465,h_322,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/90b5fe_c5aad61b20ce472ea7cc77350f978e5a~mv2_d_2651_1818_s_2.jpg');
+// pid.append(img);
---
+++ trucos.js
@@ -0,0 +1,9 @@
+while (answer !='4'){
+    
+let answer = prompt('¿cuánto es 2 + 2?');
+answer = pregunta
+} 
+
+Object.values(objeto) // convierte objetos en arrays
+Object.keys(objeto) // devuelve el nombre
+
